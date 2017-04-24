0e6851ac4efe1d139df4b56306d2a4c35685510b
simon0xia <xie.yong_hen@hotmail.com>
2017-04-24 11:01:45 +0800

    消息定义

--- Server/MessageDefine.h
+++ Server/MessageDefine.h
@@ -6,18 +6,23 @@ enum MessageType
 	CS_Heartbeat,
 	CS_Login,
 	CS_Logout,
-	CS_Chat_text = 50,
+	CS_get_OwnInfo = 50,
+	CS_get_friend,
+	CS_Chat_text = 100,
 	CS_Chat_voice,
 	CS_Chat_img,
 
 	/*客户端发送给服务端的最大消息ID*/
 	Max_CS_MSG,
 
-
-	SC_Heartbeat=500,
+	SC_Heartbeat = 500,
 	SC_Login,
 	SC_Logout,
-	SC_Chat_text=550,
+	SC_participant_join,
+	SC_participant_left,
+	SC_get_OwnInfo = 550,
+	SC_get_friend,
+	SC_Chat_text = 600,
 	SC_Chat_voice,
 	SC_Chat_img,
 
